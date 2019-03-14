3280825e8d9814f5b5dee8b642623ac75e904381
lancearena15 <lance.arena15@stjohns.edu>
2019-03-13 23:24:02 -0400

    Inital Commit
    Intial Commit

--- workspace/app.py
+++ workspace/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash,redirect
 from flask_login import LoginManager
 from workspace.config import Config
 from workspace.forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 login = LoginManager(app)
 
@@ -14,7 +19,11 @@ def index():
     return render_template("home.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
