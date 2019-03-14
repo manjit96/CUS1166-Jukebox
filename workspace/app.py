dbaf48b10bc7d16e92d30bbfe25a87ced91614b6
lancearena15 <lance.arena15@stjohns.edu>
2019-03-14 11:36:47 -0400

    Inital Branch Commit

--- workspace/app.py
+++ workspace/app.py
@@ -1,29 +1,12 @@
-from flask import Flask, render_template, flash,redirect
-from flask_login import LoginManager
-from workspace.config import Config
-from workspace.forms import LoginForm
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from workspace.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
-
 login = LoginManager(app)
-
-
-@app.route("/")
-def index():
-    return render_template("home.html")
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
-    return render_template('login.html', title='Sign In', form=form)
+login.login_view = 'login'
